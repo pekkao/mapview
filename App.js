8e3b6f4a4a609ca7250c10c05364418d2238cc36
Pekka Ojala <pojala@oamk.fi>
2023-02-15 08:34:15 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+
+const INITIAL_LATITUDE = 65.0800;
+const INITIAL_LONGITUDE = 25.4800;
+const INITIAL_LATITUDE_DELTA = 0.0922;
+const INITIAL_LONGITUDE_DELTA = 0.0421;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [latitude, setLatitude] = useState(INITIAL_LATITUDE);
+  const [longitude, setLongitude] = useState(INITIAL_LONGITUDE);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      try {
+        if (status !== 'granted') {
+          setIsLoading(false);
+          console.log('Geolocation failed');
+          return;
+        }
+        const location = await Location.getLastKnownPositionAsync(
+          {accuracy: Location.Accuracy.High});
+        setLatitude(location.coords.latitude);
+        setLongitude(location.coords.longitude);
+        setIsLoading(false);
+      }
+      catch(error) {
+        alert(error);
+        setIsLoading(false);
+      }
+    })();
+  }, [])
+
+  if (isLoading) {
+    return <View style={styles.container}>
+      <Text>Retrieving location...</Text>
+      </View>
+  }
+  else {
+    return (
+      <View style={styles.container}>
+        <MapView 
+          style={styles.map}
+          initialRegion={{
+            latitude: INITIAL_LATITUDE,
+            longitude: INITIAL_LONGITUDE,
+            latitudeDelta: INITIAL_LATITUDE_DELTA,
+            longitudeDelta: INITIAL_LONGITUDE_DELTA
+          }}
+          // mapType="satellite"
+          >
+          <Marker
+            title="Testing"
+            coordinate={{latitude: latitude, longitude: longitude}}
+            >
+          </Marker>
+        </MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height - Constants.statusBarHeight
+  }
 });
